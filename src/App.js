015d0cea00ca68d607d4d2da383800b0dd81e8f7
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-06-08 11:13:32 +0700

    Use axios to send request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import axios from "axios";
 import { Component } from "react";
 import "./App.css";
 import Users from "./components/users/Users";
 import Navbar from "./layout/Navbar";
 
 class App extends Component {
+  state = {
+    usersData: [],
+  };
+
+  componentDidMount() {
+    console.log("App component is mounted ...");
+    axios.get("https://api.github.com/users").then((response) => {
+      console.log(response.data);
+      this.setState({
+        usersData: response.data,
+      });
+    });
+  }
   render() {
     return (
       <div>
         <Navbar />
         <div className="container">
-          <Users />
+          <Users usersData={this.state.usersData} />
         </div>
       </div>
     );
