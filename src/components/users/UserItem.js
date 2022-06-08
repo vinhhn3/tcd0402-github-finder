05c83193541f5ac6edc02c9a0a40d06b96a284aa
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-06-08 11:20:50 +0700

    Convert Navbar and UserItem to functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,22 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class UserItem extends Component {
-  render() {
-    return (
-      <div className="card text-center">
-        <img
-          src={this.props.user.avatar_url}
-          className="round-img"
-          style={{ width: "60px" }}
-          alt="avatar"
-        />
-        <h3>{this.props.user.login}</h3>
-        <a href={this.props.user.html_url} className="btn btn-primary btn-sm">
-          More
-        </a>
-      </div>
-    );
-  }
-}
+const UserItem = (props) => {
+  const { avatar_url, login, html_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        className="round-img"
+        style={{ width: "60px" }}
+        alt="avatar"
+      />
+      <h3>{login}</h3>
+      <a href={html_url} className="btn btn-primary btn-sm">
+        More
+      </a>
+    </div>
+  );
+};
 
 export default UserItem;
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <div className="navbar bg-primary">Hello TCD0402</div>
-      </div>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-primary">Hello TCD0402</div>
+    </div>
+  );
+};
 
 export default Navbar;
